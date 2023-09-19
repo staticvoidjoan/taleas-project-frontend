eecaee1a387fa3501a03f0ec21feffafa5ad684c
Mario Domi <mariodomi@tuta.io>
2023-09-19 15:34:17 +0200

    add a new commponent text

---
+++ src/components/text/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+const Text = ({ text, family, weight, size, color, lineheight }) => {
+  return (
+    <div className={`${family} ${weight} ${size} ${color} ${lineheight}`}>
+      {text}
+    </div>
+  );
+};
+export default Text;
