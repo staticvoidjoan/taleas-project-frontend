7c81e27df521f813bbd67db222471cf519e42cea
Joan Shamet <shametijoan@gmail.com>
2023-09-20 12:31:01 +0200

    app js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <BrowserRouter>
-      {" "}
-      <div className="App">
-        <Routes>
-          <Route />
-        </Routes>
-      </div>
-      ;
-    </BrowserRouter>
-=======
 import './App.css';
 import UserSignIn from "../src/service/authentication/user/UserSignIn"
 
@@ -22,7 +6,6 @@ function App() {
     <div className="App">
       <UserSignIn/>
     </div>
->>>>>>> a724fcdba4e88ffb355e064a2e030db6149841ab
   );
 }
 
