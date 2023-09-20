8f869a8f0e6e4f80b257a43a5089d8c3517a6724
Joan Shamet <shametijoan@gmail.com>
2023-09-20 16:30:16 +0200

    home create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import UserSignIn from "../src/service/authentication/user/UserSignIn"
+import Home from "./pages/home/index"
 
 function App() {
   return (
     <div className="App">
-      <UserSignIn/>
+      {/* <UserSignIn/> */}
+      <Home /> 
     </div>
   );
 }
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <div className="home-bio">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Index;
