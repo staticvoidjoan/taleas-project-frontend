3acadabcb9566897fb8011b0f62d0d8f312ec695
Joan Shamet <shametijoan@gmail.com>
2023-09-20 16:53:29 +0200

    home start

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import Text from "../../components/text/index"
+import React from "react";
+import Text from "../../components/text/index";
+import "./home.css"
 const Index = () => {
-    return (
+  return (
+    <div className="home-container">
+      <div className="home-bio">
+        <Text
+          label={"Swipe your way to your dreem career"}
+          family={"open-sans"}
+          weight={"medium"}
+          size={"s20"}
+          color={"white"}
+        />
         <div>
-            <div className="home-bio">
-                <Text/>
-            </div>
+          <button>
+            <Text
+              label={"I'm an employer"}
+              family={"open-sans"}
+              weight={"regular"}
+              size={"s14"}
+            />
+          </button>
+          <button>
+            <Text
+              label={"I'm a talent"}
+              family={"open-sans"}
+              weight={"regular"}
+              size={"s14"}
+            />
+          </button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Index;
