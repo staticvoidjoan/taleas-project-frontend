9543df9ddd5cfb70a6bdf4c4945a14a63c9a142e
Mario Domi <mariodomi@tuta.io>
2023-09-22 11:01:12 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import "./App.css";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing Routes instead of Switch
-
-import LoginPage from "./service/authentication/user/userSignIn";
-import UserSignUp from "./service/authentication/user/userSignUp";
-
-import Home from "./pages/home/home";
-=======
 import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing Routes instead of Switch
 import { Auth } from "aws-amplify";
 import {Amplify} from 'aws-amplify'
@@ -19,7 +11,6 @@ import ForgotPassword from "./service/authentication/user/forgotPassword";
 
 import Home from "./pages/home/home";
 import NavBar from "./layout/navBar/navBar";
->>>>>>> fd741f4abca449ec6fc325890ddc864fc33d40e1
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
   Amplify.configure(awsExports);
@@ -46,10 +37,6 @@ function App() {
         <NavBar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-<<<<<<< HEAD
-          <Route exact path="/signin" element={<LoginPage />} />
-          <Route exact path="/signup" element={<UserSignUp />} />
-=======
           <Route
             exact
             path={authenticated ? "/" : "/signin"}
@@ -69,7 +56,6 @@ function App() {
             path="*"
             element={<Home/>}
           />
->>>>>>> fd741f4abca449ec6fc325890ddc864fc33d40e1
         </Routes>
       </Router>
     </div>
--- src/service/authentication/user/confirmSignup.jsx
+++ src/service/authentication/user/confirmSignup.jsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Auth } from 'aws-amplify';
-import { useNavigate } from 'react-router-dom';
-import "./userSignUp.css"
-=======
 import React, { useState } from "react";
 import { Auth } from "aws-amplify";
 import "./userSignIn.css";
->>>>>>> fd741f4abca449ec6fc325890ddc864fc33d40e1
 const ConfirmSignup = ({ username }) => {
   const [code, setCode] = useState("");
   const [confirmationError, setConfirmationError] = useState(null);
@@ -17,13 +10,8 @@ const ConfirmSignup = ({ username }) => {
     e.preventDefault();
     try {
       await Auth.confirmSignUp(username, code);
-<<<<<<< HEAD
-      console.log('Successfully confirmed sign up');
-      navigate("/")
-=======
       console.log("Successfully confirmed sign up");
       // You can redirect the user or perform any other action upon successful confirmation.
->>>>>>> fd741f4abca449ec6fc325890ddc864fc33d40e1
     } catch (error) {
       console.error("Error confirming sign up", error);
       setConfirmationError(
@@ -33,21 +21,6 @@ const ConfirmSignup = ({ username }) => {
   }
 
   return (
-<<<<<<< HEAD
-    <div className='signUp-form-container'>
-      <form onSubmit={confirmSignUp} className='form-body'>
-        <label className='form__label'>Enter the code sent to your email</label>
-        <input
-          className="form__input"
-          type="text"
-          placeholder="Confirmation code"
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-          required
-        />
-        {confirmationError && <p className="error-message">{confirmationError}</p>}
-        <button type='submit' className='btn'>Submit</button>
-=======
     <div className="form-box">
     <div className="form-value">
       <h2>Confirm your account</h2>
@@ -66,7 +39,6 @@ const ConfirmSignup = ({ username }) => {
           <p className="error-message">{confirmationError}</p>
         )}
          <button className="">Confirm</button>
->>>>>>> fd741f4abca449ec6fc325890ddc864fc33d40e1
       </form>
     </div>
     </div>
