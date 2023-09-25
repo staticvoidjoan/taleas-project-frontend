de63c7503a33efad1e07789177fd3be2509cd8c3
keisidoci <keisidoci12@gmail.com>
2023-09-25 11:55:13 +0200

    card component / User Home

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import Text from '../text/text';
+import location from '../../assets/icons/location.svg';
 
-const Index = () => {
+const Card = () => {
     return (
-        <div>
-            
+        <div className='card-container'>
+           <div className='card-info'>
+                <div className='job-title'><Text label={"Job title"} /></div>
+                <div className='job-info'><Text label={"Front end developer"} /></div>
+                <div className='job-location'>
+                    <img src={location}></img>
+                    <Text label={"Tirana"} />
+                </div>
+            </div> 
+            <div className='card-buttons'>
+                <div className='cancel'></div>
+                <div className='superlike'></div>
+                <div className='like'></div>
+            </div>
         </div>
     );
 }
 
-export default Index;
+export default Card;
---
+++ src/pages/User pages/userHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../../components/cards/cards'
+
+const userHome = () => {
+  return (
+    <div>
+        <Card/>
+    </div>
+  )
+}
+
+export default userHome
