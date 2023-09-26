85e95db01acd8a6b9f407bce214bfcc682414360
Mario Domi <mariodomi@tuta.io>
2023-09-26 11:21:09 +0200

    ok

--- src/layout/navBar/Navbar2.jsx
+++ src/layout/navBar/Navbar2.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Text from "../../components/text/text";
 import "./Navbar2.css";
 import NavIcon from "../../assets/images/Group 1.png";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Auth } from "aws-amplify";
 
 const Navbar2 = (props) => {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [givenName, setGivenName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isEmployee, setIsEmployee] = useState(false);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+
+  const checkAuthenticated = async () => {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+
+      const userAttributes = user.attributes || {};
+      const userGivenName = userAttributes.given_name || "";
+      setGivenName(userGivenName);
+      const userLastName = userAttributes.family_name || "";
+      setLastName(userLastName);
+
+      // const userIsEmployee = userAttributes.isEmployee || false;
+      // setIsEmployee(userIsEmployee); // Set the isEmployee state
+    } catch (error) {
+      setAuthenticated(false);
+    }
+  };
+
   const handleTurnBack = () => {
     navigate("/");
+  };
+  const handleMenu = () => {
+    navigate("/menu");
   };
   return (
     <>
@@ -29,8 +65,19 @@ const Navbar2 = (props) => {
             color={"purple"}
           />
         </div>
+        {/* <div>
+          <img src={NavIcon} alt="Menu" onClick={handleMenu} />
+        </div> */}
         <div>
-          <img src={NavIcon} alt="" />
+          {authenticated ? (
+            <>
+              <Link>
+                {givenName} {lastName}
+              </Link>
+            </>
+          ) : (
+            <img src={NavIcon} alt="Menu" onClick={handleMenu} />
+          )}
         </div>
       </div>
     </>
