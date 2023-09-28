5fb7257506bb7e71201faf84994d0f01469b971a
Mario Domi <mariodomi@tuta.io>
2023-09-28 14:29:07 +0200

    some changes

--- src/service/firebase/firebaseSignUp.js
+++ src/service/firebase/firebaseSignUp.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState, useEffect } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db } from "../../firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
@@ -8,6 +8,20 @@ const RegisterFirebase = () => {
   const [err, setErr] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      // Check if the user is authenticated
+      if (user) {
+        // User is authenticated, you can proceed with other actions here.
+      }
+    });
+
+    return () => {
+      // Unsubscribe from the authentication state listener when the component unmounts.
+      unsubscribe();
+    };
+  }, []);
 
   const handleSubmit = async (e) => {
     setLoading(true);
@@ -19,7 +33,8 @@ const RegisterFirebase = () => {
     try {
       // Create user
       const res = await createUserWithEmailAndPassword(auth, email, password);
-
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      await updateProfile(auth.currentUser, { displayName });
       // Create user data in Firestore
       await setDoc(doc(db, "users", res.user.uid), {
         uid: res.user.uid,
@@ -41,7 +56,6 @@ const RegisterFirebase = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="display name" />
@@ -60,4 +74,3 @@ const RegisterFirebase = () => {
 };
 
 export default RegisterFirebase;
-  
