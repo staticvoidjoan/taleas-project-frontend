d63ac0ea1709c9f2484081bc753d2c3008e75f8e
Joan Shamet <shametijoan@gmail.com>
2023-09-29 20:37:03 +0200

    add photo

--- src/pages/Employer/employerProfile.jsx
+++ src/pages/Employer/employerProfile.jsx
@@ -1,30 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import unicorn from "../../assets/images/Unicorn.png";
 import SignOut from "../../service/authentication/user/userSignOut";
 import Text from "../../components/text/text";
 import "./employerProfile.css";
 import ImageUploader from "../../components/Convert/convertImage";
 import axios from "axios";
-
 const EmployerProfile = ({ employerData, employeeCheck }) => {
-  const [newEmployer, setNewEmployer] = useState({
+  const [newPhoto, setNewPhoto] = useState({
     profilePhoto: "",
-    address: employerData.address || "" ,
   });
+  useEffect(() => {
+    console.log("newPhoto:", newPhoto);
+    if (newPhoto.profilePhoto) {
+      // Automatically submit the form when a new image is selected
+      console.log("Submitting form...");
+      editEmployer();
+    }
+  }, [newPhoto]);
 
-  const editEmployer = async () => {
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      // Read the selected file as a data URL (base64)
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const base64String = event.target.result;
+        setNewPhoto({
+          ...newPhoto,
+          profilePhoto: base64String,
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const editEmployer = async (e) => {
     try {
       await axios.put(
         `https://fxb8z0anl0.execute-api.eu-west-3.amazonaws.com/prod/update-profile/${employerData._id}`,
-        newEmployer // You should pass the data you want to update in the request body
+        {
+          profilePhoto: newPhoto.profilePhoto,
+        }
       );
+      window.location.reload();
     } catch (error) {
       console.error(error);
     }
   };
 
   const cardStyle = {
-    backgroundImage: `url(${employerData.profilePhoto ?? unicorn})`,
+    backgroundImage: `url(${employerData.profilePhoto || unicorn})`,
     position: "relative",
   };
 
@@ -32,18 +57,16 @@ const EmployerProfile = ({ employerData, employeeCheck }) => {
     <div className="profile-container">
       <div className="photo-container">
         <div className="profile-profile-pic" style={cardStyle}>
+          <form className="change-photo-form">
+          <i class="fa-solid fa-plus" style={{color: "#00ff88"}}>
+            <input type="file" accept="image/*" className="custom-file-input" onChange={handleImageChange} />
+          </i>
+            
+          </form>
           <div className="gradient-overlay"></div>
         </div>
       </div>
-      <form onSubmit={editEmployer}>
-        <ImageUploader
-          currentImage={employerData.profilePhoto}
-          setImage={(base64String) =>
-            setNewEmployer({ ...newEmployer, profilePhoto: base64String })
-          } // Fixed the syntax error here
-        />
-        <button type="submit">Change</button> {/* Added type attribute */}
-      </form>
+
       <div className="profile-info-box">
         <Text
           label={employerData.companyName}
