83928cc608b8ab483fdff556192d5cda6511525b
keisidoci <keisidoci12@gmail.com>
2023-09-29 13:39:39 +0200

    fixes

--- src/pages/User pages/userInfo.jsx
+++ src/pages/User pages/userInfo.jsx
@@ -8,6 +8,7 @@ import educationpic from "../../assets/images/educ.png";
 import certification from "../../assets/images/certf.png";
 import emailpic from "../../assets/icons/email.svg";
 import link from "../../assets/icons/link.svg";
+import { format } from 'date-fns';
 import "./userInfo.css";
 import axios from "axios";
 import UserSignOut from "../../service/authentication/user/userSignOut";
@@ -120,7 +121,7 @@ const UserInfo = ({ id }) => {
               </div>
               <div className="details">
                 <Text
-                  label={`${edu.startDate} - ${edu.endDate}`}
+                  label={`${edu.startDate = format(new Date(edu.startDate), 'MMMM d, yyyy')} - ${edu.endDate = format(new Date(edu.endDate), 'MMMM d, yyyy')}`}
                   size={"s14"}
                 />
               </div>
@@ -144,7 +145,7 @@ const UserInfo = ({ id }) => {
               </div>
               <div className="details">
                 <Text
-                  label={`${cert.issueDate} - ${cert.expirationDate}`}
+                  label={`${cert.issueDate = format(new Date(cert.issueDate), 'MMMM d, yyyy')} - ${cert.expirationDate = format(new Date(cert.expirationDate), 'MMMM d, yyyy')}`}
                   size={"s14"}
                 />
               </div>
@@ -161,7 +162,9 @@ const UserInfo = ({ id }) => {
           </div>
         ))}
       </div>
+      <div className="signout">
       <UserSignOut/>
+      </div>
     </div>
   );
 };
