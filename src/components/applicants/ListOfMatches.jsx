a08687509c9c83416b88d6dcfc5f44dd11a7143e
unknown <joanajaupi01@gmail.com>
2023-09-30 17:15:27 +0200

    show chat for each Applicant once even if one has applied to many posts

--- src/components/applicants/ListOfMatches.jsx
+++ src/components/applicants/ListOfMatches.jsx
@@ -12,29 +12,51 @@ function ListOfMatches({ employer }) {
     loadAcceptedApplicants();
   }, []);
 
-  const loadAcceptedApplicants = async () => {
-    try {
-      const response = await axios.get(
-        `https://fxb8z0anl0.execute-api.eu-west-3.amazonaws.com/prod/posts/creatorNoPagination/${creatorId}`
-      );
+  // ...
 
-      if (
-        response.data &&
-        response.data.posts &&
-        Array.isArray(response.data.posts) &&
-        response.data.posts.length > 0 &&
-        response.data.posts[0].recLikes &&
-        Array.isArray(response.data.posts[0].recLikes)
-      ) {
-        setAcceptedApplicants(response.data.posts[0].recLikes);
-        console.log(response.data.posts[0].recLikes);
-      } else {
-        console.log("Data structure is not as expected.");
-      }
-    } catch (error) {
-      console.error("Error loading accepted applicants:", error);
+const loadAcceptedApplicants = async () => {
+  try {
+    const response = await axios.get(
+      `https://fxb8z0anl0.execute-api.eu-west-3.amazonaws.com/prod/posts/creatorNoPagination/${creatorId}`
+    );
+
+    if (
+      response.data &&
+      response.data.posts &&
+      Array.isArray(response.data.posts)
+    ) {
+      // Create a Set to keep track of unique keys
+      const uniqueKeys = new Set();
+      
+      // Iterate through all posts and accumulate recLikes
+      const allAcceptedApplicants = response.data.posts.reduce((accumulator, post) => {
+        if (post.recLikes && Array.isArray(post.recLikes)) {
+          post.recLikes.forEach((applicant) => {
+            // Generate a unique key based on the applicant's ID
+            const key = applicant._id;
+            
+            // Check if the key is unique before adding it
+            if (!uniqueKeys.has(key)) {
+              uniqueKeys.add(key);
+              accumulator.push(applicant);
+            }
+          });
+        }
+        return accumulator;
+      }, []);
+
+      setAcceptedApplicants(allAcceptedApplicants);
+      console.log(allAcceptedApplicants);
+    } else {
+      console.log("Data structure is not as expected.");
     }
-  };
+  } catch (error) {
+    console.error("Error loading accepted applicants:", error);
+  }
+};
+
+// ...
+
 
   const chat = (applicantId) => {
     const chatId = applicantId + "_" + creatorId;
