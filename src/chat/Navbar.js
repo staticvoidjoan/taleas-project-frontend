55c5564e80875f616ea0d9b7470ae561be702759
Joan Shameti <shametijoan@gmail.com>
2023-10-02 14:26:30 +0200

    asdas

--- src/chat/Navbar.js
+++ src/chat/Navbar.js
@@ -1,30 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./chatNavBar.css";
 import unicorn from "../assets/images/Unicorn.png";
 
-function ChatNavBar({ employer, employee,userRole,test }) {
-  
+function ChatNavBar({ employer, employee }) {
   useEffect(() => {
-    console.log("employee", userRole);
+    console.log("employee", employee);
     console.log("employer", employer);
-    console.log("what am in?", test);
+    console.log(displayName)
   }, []);
 
-  const displayName = userRole === "employee" ? employee.name : employer.companyName;
-
-  useEffect(() => {
-    console.log(displayName)
-  },[displayName])
+  const displayName = employee ? employee.name : employer.email ;
 
   return (
     <div className="chatNavBar">
-      <div
-        className="chatNavBar-image"
-        alt={`${employee.name} profile`}
-        style={{
-          backgroundImage: `url(${employee.profilePhoto ?? unicorn})`,
-        }}
-      />
+      <img className="chatNavBar-image" src={unicorn} alt="unicorn" />
       <span className="chatNavBar-name">{displayName}</span>
     </div>
   );
