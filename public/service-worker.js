c88722c105a82419c57091d58065a3c938a4d48f
Selma Lico <selmalico9@gmail.com>
2023-10-25 23:29:49 +0200

    Trying cache version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,33 +1,148 @@
-const cacheName = 'my-cache';
+const cacheName = 'my-cache-v1';
+const IDBConfig = {
+    name: 'my_awesome_idb',
+    version: 1,
+    stores: [
+        {
+            name: 'api_requests',
+            keyPath: 'url'
+        },
+        {
+            name: 'api_responses',
+            keyPath: 'url'
+        }
+    ]
+};
 
-// put the static assets and routes you want to cache here
-const filesToCache = [
-  '/',
-  '/about',
-  '/index.html',
-  '/about.html',
-  '/css/styles.css',
-  '/js/app.js',
-  '/img/logo.png'
-];
-
-// the event handler for the activate event
-self.addEventListener('activate', e => self.clients.claim());
-
-// the event handler for the install event 
-// typically used to cache assets
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName)
-    .then(cache => cache.addAll(filesToCache))
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        Promise.all([
+            caches.open(cacheName),
+            createIndexedDB(IDBConfig)
+        ])
+        .then(([cache, db]) => {
+            return cache.addAll([
+                'index.html',
+                'styles.css',
+                'app.js'
+            ]);
+        })
+    );
 });
 
-// the fetch event handler, to intercept requests and serve all 
-// static assets from the cache
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-    .then(response => response ? response : fetch(e.request))
-  )
+self.addEventListener('fetch', (event) => {
+    const { request } = event;
+
+    if (request.url.includes('/prod')) {
+        event.respondWith(getCachedOrNetworkApiResponse(request));
+    } else {
+        event.respondWith(
+            caches.match(request).then((response) => {
+                return response || fetch(request);
+            })
+        );
+    }
 });
+
+const createIndexedDB = ({ name, version, stores }) => {
+    const request = self.indexedDB.open(name, version);
+
+    return new Promise((resolve, reject) => {
+        request.onupgradeneeded = (e) => {
+            const db = e.target.result;
+
+            stores.map(({ name, keyPath }) => {
+                if (!db.objectStoreNames.contains(name)) {
+                    db.createObjectStore(name, { keyPath });
+                }
+            });
+        };
+
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    });
+};
+
+const cacheApiRequest = async (request) => {
+    const headers = [...request.headers.entries()].reduce((obj, [key, value]) => Object.assign(obj, { [`${key}`]: value }), {});
+    const body = await request.text();
+    const serialized = {
+        headers,
+        body,
+        url: request.url,
+        method: request.method,
+        mode: request.mode,
+        credentials: request.credentials,
+        cache: request.cache,
+        redirect: request.redirect,
+        referrer: request.referrer
+    };
+
+    const requestStore = await openStore(IDBConfig.stores[0], 'readwrite');
+    requestStore.add(serialized);
+};
+
+const cacheApiResponse = async (response) => {
+    try {
+        const store = await openStore(IDBConfig.stores[1], 'readwrite');
+        store.add(response);
+    } catch (error) {
+        console.log('IDB error', error);
+    }
+};
+
+const getStoreFactory = (dbName, version) => ({ name }, mode = 'readonly') => {
+    return new Promise((resolve, reject) => {
+        const request = self.indexedDB.open(dbName, version);
+
+        request.onsuccess = (e) => {
+            const db = request.result;
+            const transaction = db.transaction(name, mode);
+            const store = transaction.objectStore(name);
+
+            return resolve(store);
+        };
+
+        request.onerror = (e) => reject(request.error);
+    });
+};
+
+const openStore = getStoreFactory(IDBConfig.name, IDBConfig.version);
+
+const networkThenCache = async (request) => {
+    const { method, url } = request;
+    const requestClone = request.clone();
+
+    try {
+        const response = await fetch(request);
+        const json = await response.clone().json();
+
+        if (method === 'GET') {
+            cacheApiResponse({ url, json });
+        }
+
+        return response;
+    } catch (e) {
+        return method === 'POST' ? cacheApiRequest(requestClone) : new Response(JSON.stringify({ message: 'no response' }));
+    }
+};
+
+const getCachedApiResponse = (request) => {
+    return new Promise((resolve, reject) => {
+        openStore(IDBConfig.stores[1])
+            .then((store) => {
+                const cachedRequest = store.get(request.url);
+
+                cachedRequest.onsuccess = (e) => {
+                    return cachedRequest.result === undefined ? resolve(null) : resolve(new Response(JSON.stringify(cachedRequest.result.json)));
+                };
+
+                cachedRequest.onerror = (e) => {
+                    console.log('Cached response error', e, cachedRequest.error);
+                    return reject(cachedRequest.error);
+                };
+            });
+    });
+};
+
+const getCachedOrNetworkApiResponse = async (request) => await getCachedApiResponse(request) || networkThenCache(request);
--- public/serviceWorkerRegistration.js
+++
@@ -1,9 +0,0 @@
-if('serviceWorker' in navigator) {
-  let registration;
-
-  const registerServiceWorker = async () => {
-    registration = await navigator.serviceWorker.register('./service-worker.js');
-  };
-
-  registerServiceWorker();
-}
