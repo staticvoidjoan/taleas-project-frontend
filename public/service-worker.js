df880c9c2a038e57778d55c901e4da2b26963d4f
Selma Lico <selmalico9@gmail.com>
2023-10-26 15:24:53 +0200

    static files

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,8 +24,8 @@ self.addEventListener('install', (event) => {
             return cache.addAll([
                 'index.html',
                 '/',
-                '/src/App.js',
-                '/src/App.css'
+                '/static/js',
+                '/static/css'
             ]);
         })
     );
