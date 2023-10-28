9c36f936085b8b62d5870da53f8c71a12c31349d
unknown <joanajaupi01@gmail.com>
2023-10-28 21:25:10 +0200

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@
   
   // Define the Firebase configuration object
   const firebaseConfig = {
-    apiKey: "AIzaSyA84WWH34mZ2OtMYKv7f9lv5ytMW5oYrC4",
-    authDomain: "ccrush-169b8.firebaseapp.com",
-    projectId: "ccrush-169b8",
-    storageBucket: "ccrush-169b8.appspot.com",
-    messagingSenderId: "489390331709",
-    appId: "1:489390331709:web:f67d54733c67e033c3d754",
-    measurementId: "G-6YTP8KD4HN"
+    apiKey: "AIzaSyCl7F9QpwtRokqU2NqYr3lrABZjWzdVfjA",
+    authDomain: "test-20622.firebaseapp.com",
+    projectId: "test-20622",
+    storageBucket: "test-20622.appspot.com",
+    messagingSenderId: "684340633344",
+    appId: "1:684340633344:web:a22f6397a245d153e15de5",
+    measurementId: "G-J1E5BTTK98"
   };
   
   // Now you can use the firebaseConfig object
